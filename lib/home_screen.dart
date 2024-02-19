a788c7df12a11a3f4cc530b455082f6a6256cd66
your-githb-usernam <midomohamed61>
2024-02-19 16:08:23 +0200

    first commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,34 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Firebase Install'),
+          actions: [
+            IconButton(onPressed: () async {
+              GoogleSignIn googleSignIn = GoogleSignIn();
+              googleSignIn.disconnect();
+              await FirebaseAuth.instance.signOut();
+              Navigator.of(context).pushNamedAndRemoveUntil('login', (route) => false);
+            }, icon: Icon(Icons.exit_to_app))
+          ],
+        ),
+        body: ListView(
+          children: [
+
+          ],
+        ));
+  }
+}
